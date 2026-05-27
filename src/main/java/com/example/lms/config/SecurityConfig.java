47d8a4e74b58b3e52f7f5dc9e481c22e4aaa9ca7
Boikanyo-Mohlamonyane <Boikanyokanyo481@gmail.com>
2026-05-27 09:19:19 +0200

    update swaggers

--- src/main/java/com/example/lms/config/SecurityConfig.java
+++ src/main/java/com/example/lms/config/SecurityConfig.java
@@ -19,7 +19,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
         http.csrf(csrf->csrf.disable()
                 .authorizeHttpRequests(
-                        auth->auth.anyRequest().permitAll()));
+                        auth->auth   // ✅ Swagger UI access
+                                .requestMatchers(
+                                        "/swagger-ui/**",
+                                        "/swagger-ui.html",
+                                        "/v3/api-docs/**",
+                                        "/v3/api-docs",
+                                        "/webjars/**"
+                                ).permitAll()
+                                .anyRequest().permitAll()));
 
     return http.build();
     }
